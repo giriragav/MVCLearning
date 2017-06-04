e9dc265fe6cb9201443616db178bf85750b65f73
venkat-giri <giri.ragav@gmail.com>
2017-06-04 17:34:53 -0500

    Added Movie model and random view

---
+++ MyFlix/MyFlix/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using MyFlix.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFlix.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie(){ ID = 1, Name = "MarudhaNayagam" };
+
+            return View(movie);
+        }
+    }
+}
