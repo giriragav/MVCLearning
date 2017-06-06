b9e20a5c6cb2e64939a902381c21db323bef721b
venkat-giri <giri.ragav@gmail.com>
2017-06-05 22:38:23 -0500

    Added annotations and queried the MDF database for customer list using DBContext

--- MyFlix/MyFlix/Controllers/CustomersController.cs
+++ MyFlix/MyFlix/Controllers/CustomersController.cs
@@ -9,20 +9,32 @@ namespace MyFlix.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers;
+
             return View(customers);
         }
-        public IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>(){ new Customer(){Name = "Giri", ID = 1 }, new Customer(){Name = "Preethi", ID = 2} };
-        }
+        //public IEnumerable<Customer> GetCustomers()
+        //{
+        //    return new List<Customer>(){ new Customer(){Name = "Giri", ID = 1 }, new Customer(){Name = "Preethi", ID = 2} };
+        //}
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c=>c.ID==id);
+            var customer = _context.Customers.SingleOrDefault(c=>c.ID==id);
             if (customer == null)
                 return HttpNotFound();
 
--- MyFlix/MyFlix/Models/Customer.cs
+++ MyFlix/MyFlix/Models/Customer.cs
@@ -1,8 +1,12 @@
-﻿namespace MyFlix.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFlix.Models
 {
     public class Customer
     {
         public int ID { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
