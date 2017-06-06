0a5681629cdd5a5fa465804b116ac957e059bb86
venkat-giri <giri.ragav@gmail.com>
2017-06-05 22:01:46 -0500

    Added Code First logic

--- MyFlix/MyFlix/Models/Customer.cs
+++ MyFlix/MyFlix/Models/Customer.cs
@@ -4,5 +4,8 @@
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeID { get; set; }
     }
 }
---
+++ MyFlix/MyFlix/Models/MembershipType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFlix.Models
+{
+    public class MembershipType
+    {
+        public byte Id { get; set; }
+        public short SignUpFee { get; set; }
+        public int DurationInMonths { get; set; }
+        public byte DiscountRate { get; set; }
+    }
+}
